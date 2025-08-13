45c2e403b4b7bbcda6badea1718a44d5a463918c
Douglas Rogers <designer.drogers@gmail.com>
2025-08-13 10:00:34 -0400

    Adding netlify adapter to astro site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: /** @type {any} */ ([tailwindcss()])
+  },
+  adapter: netlify()
 });
